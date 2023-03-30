9ec963e3d9f016102dfa5e9e96e5e28e819c2d69
alejopl <alejopenalopez@gmail.com>
2023-03-30 08:55:07 +0200

    Primer commit

---
+++ src/Saludadores/Saludador.java
@@ -0,0 +1,10 @@
+package Saludadores;
+
+public class Saludador {
+
+	public static void main(String[] args) {
+		System.out.println("Hola");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author 6002627
+ *
+ */
+module EjercicioEclipse {
+}
